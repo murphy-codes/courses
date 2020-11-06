f4e4443ac649ada492f1d24627e0e6077065390d
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-11-05 19:13:48 -0800

    Section 02 - The Basics

---
+++ Udemy/the-complete-guide-to-angular-2/Section-02/my-first-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: #7e0505;
+    }
+  `]
+})
+export class AppComponent {
+}
