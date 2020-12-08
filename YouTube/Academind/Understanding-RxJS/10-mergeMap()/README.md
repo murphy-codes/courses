7626a77dea84dec041b10efda0416021f2e12706
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-12-07 19:50:37 -0800

    Vid 10 - mergeMap()

---
+++ YouTube/Academind/Understanding-RxJS/10-mergeMap()/README.md
@@ -0,0 +1,5 @@
+<img src="https://i.ytimg.com/vi/b59tcUwfpWU/maxresdefault.jpg" width="10%" height="10%">
+
+# [mergeMap()](https://www.youtube.com/watch?v=b59tcUwfpWU)
+
+mergeMap(). What is it and how may we use it?
---
+++ YouTube/Academind/Understanding-RxJS/10-mergeMap()/main.js
@@ -0,0 +1,25 @@
+var input1 = document.querySelector('#input1');
+var input2 = document.querySelector('#input2');
+
+var span = document.querySelector('span');
+
+var obs1 = Rx.Observable.fromEvent(input1, 'input');
+var obs2 = Rx.Observable.fromEvent(input2, 'input');
+
+/* obs1.subscribe(
+  (event) => span.textContent = event.target.value
+);
+
+obs2.subscribe(
+  (event) => span.textContent = event.target.value
+); */
+
+obs1.mergeMap(
+  event1 => {
+    return obs2.map(
+      event2 => event1.target.value + ' ' + event2.target.value
+    )
+  }
+).subscribe(
+  combinedValue => span.textContent = combinedValue
+);
