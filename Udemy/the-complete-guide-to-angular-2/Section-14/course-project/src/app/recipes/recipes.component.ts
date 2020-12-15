82cbde7288bbbc7476f75ecf34867ecef3a4406c
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-12-14 16:54:37 -0800

    Section 14 - Course Project - Observables

---
+++ Udemy/the-complete-guide-to-angular-2/Section-14/course-project/src/app/recipes/recipes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { RecipeService } from './recipe.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+// import { Recipe } from './recipe.model';
+
+// selectedRecipe: Recipe;
+
+// private recipeService: RecipeService
+
+    // this.recipeService.recipeSelected
+    //   .subscribe(
+    //     (recipe: Recipe) => {
+    //       this.selectedRecipe = recipe;
+    //     }
+    //   );
---
+++ Udemy/the-complete-guide-to-angular-2/Section-14/course-project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Ingredient[];
+  private ingChangeSub: Subscription;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+    this.ingChangeSub = this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.ingChangeSub.unsubscribe();
+  }
+}
+
+  // onIngredientAdded(ingredient: Ingredient) { this.ingredients.push(ingredient); }
